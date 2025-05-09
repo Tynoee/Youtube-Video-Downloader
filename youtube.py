6a7b6fe8c877576d5cdedf3a2bb546d5e858721c
Tylee <tinotendachidume@gmail.com>
2025-05-09 08:15:18 +0200

    feat: Functionality improvement

--- youtube.py
+++ youtube.py
@@ -1,18 +1,44 @@
-from pytube import YouTube
+import yt_dlp
 import tkinter as tk
 from tkinter import filedialog
 
 def download_vid(url, path):
+    # Options for downloading video
+    ydl_opts = {
+        'format': 'bestvideo+bestaudio/best',  # Get the best video + best audio
+        'outtmpl': f'{path}/%(title)s.%(ext)s',  # Specify the download path and filename
+    }
+
     try:
-        yt = YouTube(url)
-        streams = yt.streams.filter(progressive = True, file_extension = "mp4")
-        highest_res_stream = streams.get_highest_resolution()
-        highest_res_stream.download(output_path = path)
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])  # Download the video
         print("Video downloaded successfully!")
     except Exception as e:
-        print(e)
+        print(f"Error: {e}")
 
-url = "https://www.youtube.com/watch?v=ZY1f7E_2iFA"
-path = "C:/Users/Admin/Desktop/Repositories/pythonYoutube-Video-Downloader"
+# Example URL and download path
+url = "https://www.youtube.com/watch?v=B9YYkGheG0M"
+path = "C:/Users/Admin/Desktop/Repositories/python/Youtube-Video-Downloader"
 
-download_vid(url, path)      
+download_vid(url, path)
+
+# def open_file_dialog():
+#     folder = filedialog.askdirectory()
+#     if folder:
+#         print(f"Selected folder: {folder}")
+#     pass
+
+
+# if __name__ == "__main__":
+#     root = tk.Tk()
+#     root.withdraw()
+    
+#     video_url = input("Please enter a Youtube url: ")
+#     save_dir = open_file_dialog()
+    
+#     if save_dir:
+#         print("Started download...")
+#         download_vid(video_url, save_dir)
+#     else:
+#         print("Invalid save location.")
+        
